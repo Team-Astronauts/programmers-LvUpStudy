e03bbac9a2e72d3267be75c9ffd63e7a9a91eb10
Ycow <skypnal12@gmail.com>
2022-10-12 17:29:51 +0900

    add: 머쓱이보다 키 큰 사람

---
+++ yunwoo/머쓱이보다-키-큰-사람.js
@@ -0,0 +1,9 @@
+function solution(array, height) {
+  const result = array.filter((v) => v > height).length;
+
+  return result;
+  function solution(array, height) {
+    const result = array.filter(v => v > height).length
+    
+    return result
+}
