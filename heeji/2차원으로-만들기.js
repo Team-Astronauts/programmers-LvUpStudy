83aeb65a812f09d657f20430bd3a8537243c352b
heejiyang <dianoheeji@naver.com>
2022-10-18 23:40:23 +0900

    add: 피자 나눠 먹기 (2)

--- heeji/2차원으로-만들기.js
+++ heeji/2차원으로-만들기.js
@@ -1,5 +1,5 @@
 function solution (num_list, n) {
-    let newArray = [];
+    const newArray = [];
     for (i = 0; i < num_list.length; i + n) {
         newArray.push(num_list.splice(i, n));
     }
