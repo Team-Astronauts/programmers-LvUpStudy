a21213d90f1dd895621153990964ec90a3d30f4f
Ocknyer <kdu4045@naver.com>
2022-10-18 21:37:51 +0900

    add: 피자 나눠 먹기 (2)

--- daeun/피자-나눠-먹기-(2).js
+++ daeun/피자-나눠-먹기-(2).js
@@ -1,10 +1,11 @@
 function solution(n) {
     const pizza = 6
     const gcd = (pizza, n) => pizza % n === 0 ? n : gcd(n, pizza % n);
-    const lcm = (pizza, n) => pizza * n / gcd(pizza, n);
+    const lcm = (pizza, n) => pizza * n / gcd(pizza, n); // 유클리드 호제법을 사용한 최소공배수 구하기
     return lcm(pizza, n)/6
 }
 
+// while 문을 활용한 방법
 function solution2(n) {
     let pizza2 = 6
     while (pizza2 % n !== 0) {
