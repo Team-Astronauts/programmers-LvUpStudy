633b4c35c99f1d2b3d62b4213ae8e2a85a8f721f
Ocknyer <kdu4045@naver.com>
2022-10-19 17:41:03 +0900

    add: 종이 자르기

---
+++ daeun/종이-자르기.js
@@ -0,0 +1,9 @@
+function solution(M, N) {
+    return (M-1) + ((N-1) * M);
+}
+
+// function solution2(M, N) {
+//     let vertical = M - 1
+//     let horizontal = (N - 1) * M
+//     return vertical + horizontal;
+// }
