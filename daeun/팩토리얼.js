b85e406c6e896db9f1d55142a466de2cf0146d0e
Ocknyer <kdu4045@naver.com>
2022-10-25 17:09:18 +0900

    fix: 팩토리얼

--- daeun/팩토리얼.js
+++ daeun/팩토리얼.js
@@ -1,6 +1,6 @@
 function solution(n) {
     let factorial = 1;
-    for (i = 1; i < 11; i++) {
+    for (let i = 1; i < 11; i++) {
         factorial *= i
         if (factorial === n) {
             return i
