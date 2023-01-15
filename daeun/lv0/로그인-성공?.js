3c3551868cc755f0f3055d553e358b5e84fc04c0
Ocknyer <kdu4045@naver.com>
2023-01-15 15:56:26 +0900

    add: 로그인 성공?

---
+++ daeun/lv0/로그인-성공?.js
@@ -0,0 +1,23 @@
+function solution(id_pw, db) {
+    let answer = "";
+
+    for (let i = 0; i < db.length; i++) {
+        if (db[i][0] === id_pw[0]) {
+            if (db[i][1] === id_pw[1]) {
+                answer += "login";
+            } else {
+                answer += "wrong pw";
+            }
+        } else {
+            answer += "fail";
+        }
+    }
+
+    if (answer.includes("login")) {
+        return "login";
+    } else if (answer.includes("wrong pw")) {
+        return "wrong pw";
+    } else {
+        return "fail";
+    }
+}
