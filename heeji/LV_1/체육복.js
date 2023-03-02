6b596f4749ba25e1238388316fde5500b5febe3d
heejiyang <dianoheeji@naver.com>
2023-03-02 15:35:17 +0900

    add: 체육복 풀이 추가

--- heeji/LV_1/체육복.js
+++ heeji/LV_1/체육복.js
@@ -19,3 +19,24 @@ function solution(n, lost, reserve) {
   });
   return answer;
 }
+
+function solution(n, lost, reserve) {
+  const clothes = Array(n).fill(1);
+  lost.map((lost) => {
+    clothes[lost - 1] = 0;
+  });
+  reserve.map((reserve) => {
+    clothes[reserve - 1] += 1;
+  });
+
+  for (let i = 0; i < n; i++) {
+    if (clothes[i] === 0 && clothes[i - 1] === 2) {
+      clothes[i] = 1;
+      clothes[i - 1] = 1;
+    } else if (clothes[i] === 0 && clothes[i + 1] === 2) {
+      clothes[i] = 1;
+      clothes[i + 1] = 1;
+    }
+  }
+  return clothes.filter((i) => i > 0).length;
+}
