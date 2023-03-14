bf0d634d1574baa683595fa2545f67d58469a1f6
heejiyang <dianoheeji@naver.com>
2023-03-14 23:29:40 +0900

    add: 둘만의 암호 문제풀이 추가

--- heeji/LV_1/둘만의-암호.js
+++ heeji/LV_1/둘만의-암호.js
@@ -12,3 +12,38 @@ function solution(s, skip, index) {
   }
   return answer;
 }
+
+function solution(s, skip, index) {
+  const alphabet = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+  ].filter((c) => !skip.includes(c));
+  return s
+    .split("")
+    .map((c) => alphabet[(alphabet.indexOf(c) + index) % alphabet.length])
+    .join("");
+}
