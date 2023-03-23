28208314f321437dab90562ee109439dbd5797e2
Ocknyer <kdu4045@naver.com>
2023-03-23 20:44:40 +0900

    add: 소수 만들기

---
+++ daeun/lv1/소수-만들기.js
@@ -0,0 +1,23 @@
+function isPrime(num) {
+  for (let i = 3; i <= Math.sqrt(num); i += 2) {
+    if (num % i === 0) return false;
+  }
+  return true;
+}
+
+function solution(nums) {
+  let answer = 0;
+
+  for (let i = 0; i < nums.length - 2; i++) {
+    for (let j = i + 1; j < nums.length - 1; j++) {
+      for (let k = j + 1; k < nums.length; k++) {
+        let sum = nums[i] + nums[j] + nums[k];
+        if (sum % 2 !== 0 && isPrime(sum)) {
+          answer++;
+        }
+      }
+    }
+  }
+
+  return answer;
+}
