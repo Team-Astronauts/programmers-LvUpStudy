e220d6a8de5ffd6acba70f0610c34980eb3d50bd
heejiyang <dianoheeji@naver.com>
2023-03-29 17:27:08 +0900

    add: JadenCase 문자열 만들기 풀이 추가

--- heeji/LV_2/JadenCase문자열-만들기.js
+++ heeji/LV_2/JadenCase문자열-만들기.js
@@ -12,3 +12,12 @@ function solution(s) {
     .join(" ");
   return answer;
 }
+
+// chartAt(0) : 입력한 글자의 1번째 문자
+// substring(1) : 문자열 자르기. 반환 문자열의 시작 인덱스. index 1부터 끝까지의 문자열 return
+function solution(s) {
+  return s
+    .split(" ")
+    .map((v) => v.charAt(0).toUpperCase() + v.substring(1).toLowerCase())
+    .join(" ");
+}
