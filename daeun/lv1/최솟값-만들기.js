331f69d016c8231ad2178b85e3dfef483667d4ea
Ocknyer <kdu4045@naver.com>
2023-04-04 23:31:58 +0900

    add: 최솟값 만들기 풀이 추가

--- daeun/lv1/최솟값-만들기.js
+++ daeun/lv1/최솟값-만들기.js
@@ -10,3 +10,11 @@ function solution(A, B) {
 
   return answer;
 }
+
+// reduce를 활용한 풀이
+function solution2(A, B) {
+  const sortedA = A.sort((a, b) => a - b);
+  const sortedB = B.sort((a, b) => b - a);
+
+  return sortedA.reduce((acc, cur, idx) => acc + cur * B[idx], 0);
+}
