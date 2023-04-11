51372e7d346bff995ffd32cb1654a9d1bf00d7c6
heejiyang <dianoheeji@naver.com>
2023-04-12 01:52:36 +0900

    add: 영어 끝말잇기 풀이 추가

--- heeji/LV_2/영어-끝말잇기.js
+++ heeji/LV_2/영어-끝말잇기.js
@@ -13,3 +13,25 @@ function solution(n, words) {
 
   return [0, 0];
 }
+
+function solution(n, words) {
+  let answer = [0, 0];
+  let usedWords = [];
+  usedWords.push(words[0]);
+  for (let i = 1; i < words.length; i++) {
+    let beforeWord = words[i - 1];
+    let currentWord = words[i];
+    if (
+      beforeWord[beforeWord.length - 1] == currentWord[0] &&
+      usedWords.indexOf(currentWord) == -1
+    ) {
+      // 계속 게임 진행
+      usedWords.push(currentWord);
+    } else {
+      let turn = parseInt(i / n) + 1;
+      let player = (i % n) + 1;
+      return (answer = [player, turn]);
+    }
+  }
+  return answer;
+}
